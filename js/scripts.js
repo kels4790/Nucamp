493a030c16f8ed5b41bd89e35a0dcd10f9588276
LeslieHarris <kels4790@gmail.com>
2022-07-12 18:55:26 -0400

    sample project

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(function() { 
+    $('#reserveButton').click(function() {
+    $('#reserveModal').modal('show'); 
+ 
+    }) 
+    
+});
+
+$(function() { 
+    $('#loginButton').click(function() {
+    $('#loginModal').modal('show'); 
+ 
+    }) 
+    
+});
+
+
---
+++ js/scripts1.js
@@ -0,0 +1,19 @@
+(function() {
+    $(".carousel").carousel( { interval: 2000 } );
+    $("#carouselButton").on("click", fn)() {
+        $(".carousel").trigger("click"),
+            if ($("#carouselButton").children("i").hasClass("fa-pause")){
+                $(".carousel").carousel("pause");
+                $("#carouselButton").children("i").removeClass("fa-pause");
+                $("#carouselButton").children("i").addClass("fa-play");
+
+            } else {
+                $(".carousel").carousel("cycle");
+                $("#carouselButton").children("i").removeClass("fa-play");
+                $("#carouselButton").children("i").addClass("fa-pause");
+            }
+    });
+}) 
+
+
+
